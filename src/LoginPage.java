4586ad4067f253399676fba80fd786153cf24003
Mohammad Nasser <mhmdnaser1572@gmail.com>
2024-10-15 10:09:12 +0330

    setting bounds for userIDLabel , filed , login button

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -21,24 +21,24 @@ public class LoginPage implements ActionListener {
     JLabel messageLabel = new JLabel();
     HashMap<String, String> logininfo = new HashMap<String, String>();
 
-    LoginPage(HashMap loginInfoOriginal){
+    LoginPage(HashMap loginInfoOriginal) {
 
         logininfo = loginInfoOriginal;
 
-        userIDLabel.setBounds(50,100,75,25);
-        userPasswordLabel.setBounds(50,150,75,25);
+        userIDLabel.setBounds(50, 100, 75, 25);
+        userPasswordLabel.setBounds(50, 150, 75, 25);
 
-        messageLabel.setBounds(125,250,250,35);
+        messageLabel.setBounds(125, 250, 250, 35);
         messageLabel.setFont(new Font(null, Font.ITALIC, 25));
 
-        userIDField.setBounds(125,100,200,25);
-        userPasswordField.setBounds(125,150,200,25);
+        userIDField.setBounds(125, 100, 200, 25);
+        userPasswordField.setBounds(125, 150, 200, 25);
 
-        loginButton.setBounds(125,200,100,25);
+        loginButton.setBounds(125, 200, 100, 25);
         loginButton.setFocusable(false);
         loginButton.addActionListener(this);
 
-        resetButton.setBounds(225,200,100,25);
+        resetButton.setBounds(225, 200, 100, 25);
         resetButton.setFocusable(false);
         resetButton.addActionListener(this);
 
@@ -55,34 +55,31 @@ public class LoginPage implements ActionListener {
         frame.setVisible(true);
 
 
-
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==resetButton){
+        if (e.getSource() == resetButton) {
             userIDField.setText("");
             userPasswordField.setText("");
         }
 
-        if(e.getSource()==loginButton){
+        if (e.getSource() == loginButton) {
             String userID = userIDField.getText();
             String password = String.valueOf(userPasswordField.getPassword());
 
-            if(logininfo.containsKey(userID)){
-                if(logininfo.get(userID).equals(password)){
+            if (logininfo.containsKey(userID)) {
+                if (logininfo.get(userID).equals(password)) {
                     messageLabel.setForeground(Color.green);
                     messageLabel.setText("Login successful");
                     frame.dispose();
                     @SuppressWarnings("unused")
                     WelcomePage welcomePage = new WelcomePage(userID);
-                }
-                else{
+                } else {
                     messageLabel.setForeground(Color.red);
                     messageLabel.setText("Wrong password, try again");
                 }
-            }
-            else{
+            } else {
                 messageLabel.setForeground(Color.red);
                 messageLabel.setText("User does not exist");
             }
